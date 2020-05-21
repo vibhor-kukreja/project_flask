99a453b044dbbec31ea496d6b1b218ffa4ea63b1
Aman Jaiswal <amanjai01@gmail.com>
2020-05-21 23:28:25 +0530

    Required changes in constants and json for seed script

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,6 +1,11 @@
 """Constant file for Utils"""
+
+
+class DisplayMessage(object):
+    WRITING_SEED_DATA = "Writing seed data for {}"
+    WRITING_SUCCESSFUL = "Writing successful for {}"
+
+
 POOL_CHUNK_SIZE = 100
 APP_NAME = "my_flask_app"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
-WRITING_SEED_DATA = "Writing seed data for {}"
-WRITING_SUCCESSFUL = "Writing successful for {}"
