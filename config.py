3daca0e20461d4ebbcbf8bce2e235686c83b7d2f
vibhorkukreja <vibhor.kukreja@tothenew.com>
2020-05-22 13:36:26 +0530

    Refactored the env workflow.

--- config.py
+++ config.py
@@ -1,79 +1,53 @@
 """Main configuration file"""
 # Define the application directory
-import os
+from os import getenv, path
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+current_env = getenv("FLASK_ENV") or 'local'
+if not path.exists("{}.env".format(current_env)):
+    raise EnvironmentError("FLASK_ENV not set properly for {} env.".format(
+        current_env))
+
+# loading the selected .env file
+project_folder = path.expanduser(BASE_DIR)
+load_dotenv(path.join(project_folder, "{}.env".format(current_env)))
 
 
-class LocalConfig(object):
-    """
-    This is a class for base configuration for the project
-    """
-
-    # Statement for enabling the development environment
-    DEBUG = True
-
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-    # Define the database - we are working with
-    # SQLite for this example
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "app.db")
-    # To disable tracking modifications on Objects by Flask-SQLAlchemy
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE_CONNECT_OPTIONS = {}
-
-    # Application threads. A common general assumption is
-    # using 2 per available processor cores - to handle
-    # incoming requests using one and performing background
-    # operations using the other.
-    THREADS_PER_PAGE = 2
-
-    # Enable protection again *Cross-site Request Forgery (CSRF)*
-    CSRF_ENABLED = True
-
-    # Use a secure, unique and absolutely secret key for
-    # signing the data.
-    CSRF_SESSION_KEY = "secret"
-
-    # Secret key for signing cookies
-    SECRET_KEY = "secret"
-
-    AUTH_TOKEN_TTL_MINUTES = 60
-    JWT_SECRET_KEY = "M0n3Y_H3!5T"
-
-    HOOKS_REQUIRED = True
-
-    # Logs
-    LOG_LEVEL = 'DEBUG'
-    LOG_FILE_PATH = "app/logger/logs"
+# ######################## #
+# #### Configurations #### #
+# ######################## #
 
 
-class StagingConfig(LocalConfig):
-    """
-    Class containing configuration for staging environment
-    """
-    SECRET_KEY = "staging-secret"
-    CSRF_SESSION_KEY = "staging-csrf-secret"
-    DEBUG = False
+DEBUG = getenv('DEBUG')
 
-    # Logs
-    LOG_LEVEL = 'DEBUG'
-    LOG_FILE_PATH = "app/logger/logs"
+SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+# To disable tracking modifications on Objects by Flask-SQLAlchemy
+SQLALCHEMY_TRACK_MODIFICATIONS = getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+DATABASE_CONNECT_OPTIONS = getenv('DATABASE_CONNECT_OPTIONS')
 
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = getenv('THREADS_PER_PAGE')
 
-class ProductionConfig(LocalConfig):
-    """
-    Class containing configuration for production environment
-    """
-    SECRET_KEY = "prod-secret"
-    CSRF_SESSION_KEY = "production-csrf-secret"
-    DEBUG = False
+# Enable protection again *Cr-site Request Forgery (CSRF)*
+CSRF_ENABLED = getenv('CSRF_ENABLED')
 
-    # Logs
-    LOG_LEVEL = 'INFO'
-    LOG_FILE_PATH = "app/logger/logs"
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = getenv('CSRF_SESSION_KEY')
 
+# Secret key for signing cookies
+SECRET_KEY = getenv('SECRET_KEY')
 
-APP_ENV_CONFIGS = {
-    "local": "config.LocalConfig",
-    "production": "config.ProductionConfig",
-    "staging": "config.StagingConfig"
-}
+AUTH_TOKEN_TTL_MINUTES = getenv('AUTH_TOKEN_TTL_MINUTES')
+JWT_SECRET_KEY = getenv('JWT_SECRET_KEY')
+
+HOOKS_REQUIRED = getenv('HOOKS_REQUIRED')
+
+# Logs
+LOG_LEVEL = getenv('LOG_LEVEL')
+LOG_FILE_PATH = getenv('LOG_FILE_PATH')
